ad7c8a5ae146c9838b633c2863ea567d5d9ff3cf
Karthik G <karthik.katy@gmail.com>
2017-12-11 23:06:24 +0530

    Add all the files to the commit

---
+++ README.md
@@ -0,0 +1,100 @@
+**HPDF WEEK ONE TASK**
+------------------
+
+
+----------
+
+
+###Requirements
+
+
+
+
+ Python 3
+ pip
+
+
+----------
+
+
+###Installation Instructions
+Download python from https://www.python.org/downloads/release/python-363/ and install.
+Add path in enivironment varibles in windows System Settings as `C:\Python362\` and `C:\Python362\scripts`
+Downlad pip from https://bootstrap.pypa.io/get-pip.py and run the script.
+
+Goto command prompt and run these commands
+
+    pip install virtualenv
+    pip install virtualenvwrapper-win
+
+Create a virtualenv :
+
+ 
+
+    mkvirtualenv project_name
+
+ Now virtual environment is active  and anything we install now will be specific to this project and install Flask and other frameworks by running these commands in command prompt
+
+ 
+
+    pip install flask
+    pip install flask-wtf
+    pip install requests
+     
+
+Create a folder and change Directory
+ 
+
+    mkdir folder_name
+    setprojectdir .
+
+Now clone this Repository and run the application
+
+    python weekone.py
+
+
+----------
+
+
+Tasks:
+
+
+For execution access  urls in your browser.
+
+1. A simple hello-world at http://localhost:8080/ that displays a simple string like "Hello World"
+
+		http://localhost:5000
+
+2. Add a route, for e.g. http://localhost:8080/authors, which:
+
+	a. Fetches a list of authors from a request to https://jsonplaceholder.typicode.com/users
+
+		http://localhost:5000/users
+
+	b. Fetches a list of posts from a request to https://jsonplaceholder.typicode.com/posts
+
+		http://localhost:5000/posts
+
+	c. Respond with only a list of authors and the count of their posts (a newline for each author).
+
+		http://localhost:5000/posts
+
+3. Set a simple cookie (if it has not already been set) at http://localhost:8080/setcookie with the following values: name=<your-first-name> and age=<your-age>.
+
+	    http://localhost:5000/setcookie
+
+4. Fetch the set cookie with http://localhost:8080/getcookies and display the stored key-values in it.
+
+		http://localhost:5000/getcookies
+
+5. Deny requests to your http://localhost:8080/robots.txt page. (or you can use the response at http://httpbin.org/deny if needed)
+
+		http://localhost:5000/robots.txt
+
+6. Render an HTML page at http://localhost:8080/html or an image at http://localhost:8080/image
+
+		http://localhost:5000/html
+
+7. A text box at http://localhost:8080/input which sends the data as POST to any endpoint of your choice. This endpoint should log the received the received to stdout.
+
+		http://localhost:5000/input
---
+++ weekone.py
@@ -0,0 +1,129 @@
+import requests
+from flask import Flask, render_template, url_for, request, redirect, flash, make_response, abort
+from logging import DEBUG
+
+from flask_wtf import Form
+from wtforms.fields import StringField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, email
+app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\xb7\xd8\xa0\x8b\x82\r\xa4W\xb00\x13s\x00\x1e\xd6hT\xc3F@3\xff<\x1e'
+
+app.logger.setLevel(DEBUG)
+
+inputs=[]
+
+def store_input(name,email):
+	inputs.append(dict(
+		name = name,
+		email = email))
+
+
+# Task 1: Say Hello world
+@app.route('/')
+def task1():
+	return 'Hello World Karthik'
+
+
+# Task 2.a: fetches a list of authors
+@app.route('/users')
+def task2a():
+	authors,posts={},{}
+	# Task 2.a: fetches a list of authors
+	authors_url = 'https://jsonplaceholder.typicode.com/users'
+	author_data= requests.get(authors_url).json()
+	for i in author_data:
+		authors[i['id']]=i['name']
+	return render_template('users.html',authors=authors)
+
+
+# Task 2.b: fetches a list of posts
+@app.route('/posts')
+def task2b():
+	posts_url = 'https://jsonplaceholder.typicode.com/posts'
+	posts_data= requests.get(posts_url).json()
+	posts =[]
+	for i in posts_data:
+		posts.append(i['title'])
+	return render_template('posts.html',posts=posts)
+
+
+#Task 3: Return A list of authors and the count of their posts
+@app.route('/authors')
+def task2c():
+	authors,posts={},{}
+	author_data= requests.get('https://jsonplaceholder.typicode.com/users').json()
+	for i in author_data:
+		authors[i['id']]=i['name']
+
+	posts_data= requests.get('https://jsonplaceholder.typicode.com/posts').json()
+	for i in posts_data:
+		if i['userId'] not in posts:
+			posts[i['userId']] = [i['title']]
+		else:
+			posts[i['userId']].append(i['title'])
+
+	authors_posts_count=[]
+	number_of_authors=len(authors)
+	for i in range(1,number_of_authors+1):
+		authors_posts_count.append([authors[i],len(posts[i])])
+	return render_template('authors.html',authors_n_posts = authors_posts_count) 
+
+
+#Task 3: Setting a Cookie
+@app.route('/setcookie')
+def task3():
+	resp = make_response(render_template('setcookie.html'))
+	resp.set_cookie('name', b'Karthik')
+	resp.set_cookie('age', b'26')
+	return resp
+
+
+# Task 4: Display the stored cookies
+@app.route('/getcookies')
+def task4():
+	name=request.cookies.get('name')
+	age=request.cookies.get('age')
+	return render_template('getcookies.html',name = name, age = age)
+
+# Task 5: Deny requests to robots.txt
+@app.route('/robots.txt')
+def task5():
+	abort(401)
+
+# Task 6: Rendering Html page
+@app.route('/html')
+def task6():
+	return render_template('html.html')
+
+
+class InputForm(Form):
+	"""docstring for InputForm"""
+	name = StringField('name', validators = [DataRequired()])
+	email = EmailField('email', validators = [DataRequired(), email()])	
+
+
+@app.route('/input',methods=['GET','POST'])
+def task7():
+	form = InputForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		email = form.email.data
+		store_input(name, email)
+		#logging the input to
+		app.logger.debug('Submitted Successfully :-)\nName: '+name +'\nEmail : '+ email)
+		flash('Submitted Successfully :-) Name: '+name +' | Email : '+ email)
+		return redirect(url_for('task6'))
+	return render_template('input.html',form = form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(401)
+def access_denied(e):
+	return render_template('401.html'), 401
+
+if __name__ == '__main__':
+	app.run(debug = True)
